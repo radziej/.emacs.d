6a4980e88835cc0959bd6c66bb6cd227b4f2838e
Markus Radziej <markus.radziej@gmail.com>
2013-12-20 16:22:48 +0100

    font to consolas

--- init.el
+++ init.el
@@ -9,8 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu LGC Sans Mono" :foundry "unknown" :slant normal :weight normal :height 101 :width normal))))
-)
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 98 :width normal)))))
 
 ;; load various configuration files
 (load "~/.emacs.d/lisp/packages")
